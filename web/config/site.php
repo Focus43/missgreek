4940ccfe1c71ceba2b0f920aa4d677d41e4ad2bd
Jonathan Hartman <jhartman86@gmail.com>
2015-04-10 10:04:08 -0600

    Retry

--- web/config/site.php
+++ web/config/site.php
@@ -10,8 +10,8 @@
 		
 		// needed for successful installation on Pagodabox. see issue
 		// http://www.concrete5.org/developers/bugs/5-6-0-2/install-fails-with-mysql-auto-increment-offset-set/
-		define('REGISTERED_GROUP_ID', '5');
-		define('ADMIN_GROUP_ID', '9');
+		// define('REGISTERED_GROUP_ID', '5');
+		// define('ADMIN_GROUP_ID', '9');
 		
 		// connect to Redis cache
 		define('REDIS_CONNECTION_HANDLE', 'tunnel.pagodabox.com:6379');
@@ -27,6 +27,14 @@
 
         // disable marketplace support b/c of Pagodabox read-only file system
         define('ENABLE_MARKETPLACE_SUPPORT', false);
+
+        // thumbnail compression defaults
+        define('AL_THUMBNAIL_JPEG_COMPRESSION', 90);
+
+        // AUTHORIZE.NET STUFF (CLINICA SPECIFIC)
+        define('AUTHORIZENET_API_LOGIN_ID', $_SERVER['AUTHNET_API_LOGIN']);
+        define('AUTHORIZENET_TRANSACTION_KEY', $_SERVER['AUTHNET_API_TRXN_KEY']);
+        define('AUTHORIZENET_SANDBOX', false);
 	
 	/**
 	 * STAGING, LOCAL MACHINE, OR VAGRANT?
@@ -63,6 +71,11 @@
                 define('PAGE_TITLE_FORMAT', '%2$s');
             }
 
+            // AUTHORIZE.NET STUFF (TEST ACCOUNT CREDENTIALS)
+            define('AUTHORIZENET_API_LOGIN_ID', '7ep7L4U4');
+            define('AUTHORIZENET_TRANSACTION_KEY', '4y4G4436kMYJg749');
+            define('AUTHORIZENET_SANDBOX', true);
+
         }else{
 
             require __DIR__ . '/site.local.php';
@@ -84,16 +97,18 @@
 
 	}
 
-
-    /**
-     * Database connection settings are passed as environment variables, from
-     * a) Pagodabox; b) Vagrant; c) manually if site.local.php exists
-     */
-    define('DB_SERVER',     $_SERVER['DB1_HOST']);
-    define('DB_USERNAME',   $_SERVER['DB1_USER']);
-    define('DB_PASSWORD',   $_SERVER['DB1_PASS']);
-	define('DB_DATABASE',   $_SERVER['DB1_NAME']);
-	define('PASSWORD_SALT', '6NVukfgwAgqaOi3SMlsWwEqURSe4Xh8pBApvhOauP7blC2kx1FKsHxcjGSXMqP3N');
+    // server variables are set by Pagoda, or by you in site.local.php
+    define('DB_SERVER',     $_SERVER['DATABASE1_HOST']);
+    define('DB_USERNAME',   $_SERVER['DATABASE1_USER']);
+    define('DB_PASSWORD',   $_SERVER['DATABASE1_PASS']);
+    define('DB_DATABASE',   $_SERVER['DATABASE1_NAME']);
+    define('PASSWORD_SALT', '6NVukfgwAgqaOi3SMlsWwEqURSe4Xh8pBApvhOauP7blC2kx1FKsHxcjGSXMqP3N');
 	
 	// sitemap.xml file
 	define('SITEMAPXML_FILE', 'files/sitemap.xml');
+
+    // issue emails from address
+    define('OUTGOING_MAIL_ISSUER_ADDRESS', 'webreceipt@clinica.org');
+    define('EMAIL_DEFAULT_FROM_ADDRESS', OUTGOING_MAIL_ISSUER_ADDRESS);
+    define('EMAIL_ADDRESS_FORGOT_PASSWORD', OUTGOING_MAIL_ISSUER_ADDRESS);
+    define('EMAIL_DEFAULT_FROM_NAME', 'Clinica.org Website');
